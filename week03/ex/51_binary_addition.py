c2a569447f257cd1b78bc56eab270e631738dd3c
thenghuy <keolythenghuy21@kit.edu.kh>
2022-08-03 20:47:10 +0700

    week 3 (2)done

--- week03/ex/51_binary_addition.py
+++ week03/ex/51_binary_addition.py
@@ -1,10 +1,33 @@
 def binary_addition(num1,num2):
     binary_num_1=bin(num1).replace("0b","")
     binary_num_2=bin(num2).replace("0b","")
-    sum=num1+num2
-    binary_sum=bin(sum).replace("0b","")
-    print("Num1         :  {}\n"
-          "Num2         :  {}\n"
-          "Sum (Binary) :  {}\n"
-          "Sum (Decimal):  {} ".format(binary_num_1,binary_num_2,binary_sum,sum))
+    list=[]
+    c="0"
+    maxlength=max(len(binary_num_1),len(binary_num_2))
+    binary_num_1=binary_num_1.zfill(maxlength)
+    binary_num_2=binary_num_2.zfill(maxlength)
+    for i in range(maxlength-1,-1,-1):
+           b1=binary_num_1[i]
+           b2 = binary_num_2[i]
+           if b1=="0" and b2=="0" and c=="0":
+             list.append("0")
+             c="0"
+           elif b1 == "1" and b2 == "1" and c == "1":
+             list.append("1")
+             c = "1"
+           elif (b1 == "1" and b2 == "0" and c == "0")or (b1 == "0" and b2 == "1" and c == "0") or (b1 == "0" and b2 == "0" and c == "1"):
+             list.append("1")
+             c = "0"
+           else:
+             list.append("0")
+             c = "1"
+    if c=="1":
+           list.append("1")
+    result="".join(list[::-1])
+    print("Num1                :  {}\n"
+          "Num2                :  {}\n"
+          "Difference (Binary) :  {}\n"
+          "Difference (Decimal):  {} ".format(binary_num_1,binary_num_2,result,num1+num2))
 binary_addition(60,50)
+
+
--- week03/ex/52_binary_substraction.py
+++ week03/ex/52_binary_substraction.py
@@ -1,10 +1,38 @@
 def binary_substraction(num1,num2):
-    binary_of_num1=bin(num1).replace("0b","")
-    binary_of_num2 = bin(num2).replace("0b", "")
-    substraction=num1-num2
-    binary_of_sunstraction=bin(substraction).replace("0b", "")
+    binary_num_1=bin(num1).replace("0b","")
+    binary_num_2 = bin(num2).replace("0b", "")
+    list = []
+    c = "0"
+    maxlength = max(len(binary_num_1), len(binary_num_2))
+    binary_num_1 = binary_num_1.zfill(maxlength)
+    binary_num_2 = binary_num_2.zfill(maxlength)
+    for i in range(maxlength-1,-1,-1):
+           b1=binary_num_1[i]
+           b2 = binary_num_2[i]
+           if (b1=="0" and b2=="0" and c=="0") or  (b1=="1" and b2=="1" and c=="0") :
+             list.append("0")
+             c="0"
+           elif (b1=="0" and b2=="1" and c=="1"):
+               list.append("0")
+               c = "1"
+
+           elif (b1=="0" and b2=="1" and c=="0") or(b1=="1" and b2=="1" and c=="1")or (b1=="0" and b2=="0" and c=="1"):
+               list.append("1")
+               c = "1"
+           elif (b1=="1" and b2=="0" and c=="0") :
+               list.append("1")
+               c = "0"
+           elif b1=="1" and b2=="0" and c=="1" :
+               list.append("0")
+               c = "0"
+    if c == "11":
+        list.append("1")
+    result="".join(list[::-1])
     print("Num1                :  {}\n"
           "Num2                :  {}\n"
           "Difference (Binary) :  {}\n"
-          "Difference (Decimal):  {} ".format(binary_of_num1,binary_of_num2,binary_of_sunstraction,substraction))
+          "Difference (Decimal):  {} ".format(binary_num_1, binary_num_2, result.lstrip("0"), num1- num2))
 binary_substraction(60, 50)
+
+
+
