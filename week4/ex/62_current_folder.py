e23639346eaa0d84e246e6cfb7c9fc351f656cae
thenghuy <keolythenghuy21@kit.edu.kh>
2022-08-05 10:26:02 +0700

    week 4 (1)done

--- week4/ex/62_current_folder.py
+++ week4/ex/62_current_folder.py
@@ -1,10 +1,12 @@
 import os
-list=[]
-for (root, dirs, files) in os.walk("C:/Users/keoly/OneDrive/Desktop/Python Bootcamp 2022/week4"):
+def current_folder(current_path):
+ list=[]
+ for (root, dirs, files) in os.walk(current_path):
     for i in range(len(files)):
            x=files[i], " files "
            list.append(x)
     for j in range(len(dirs)):
            y=dirs[j],"folder"
            list.append(y)
-print(list)
+ print(list)
+current_folder("C:/Users/keoly/OneDrive/Desktop/Python Bootcamp 2022/week4")
